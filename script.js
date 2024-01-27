a975b6a44c9acd875dc20c602af94001214768e2
PoojaDumpala <poojad033199@gmail.com>
2024-01-27 17:06:14 -0600

    Quiz is ready

--- script.js
+++ script.js
@@ -28,7 +28,72 @@ const questions = [
             { text: "To invoke a static method", correct: false},
             { text: "To access a static variable", correct: false},
         ]
+    },
+    {
+        question: "Which statement is used to remove all records from a table in MySQL?",
+        answers: [
+            { text: "Delete All", correct: false},
+            { text: "Remove", correct: false},
+            { text: "Truncate Table", correct: true},
+            { text: "Drop Table.", correct: false}, 
+        ]
+    },
+    {
+        question: "Which data type is used to store date and time values in MySQL?",
+        answers: [
+            { text: "Int", correct: false},
+            { text: "Varchar", correct: false},
+            { text: "DateTime", correct: true},
+            { text: "Float", correct: false},  
+        ]
+    },
+    {
+        question: "How can you insert data into a MySQL table?",
+        answers: [
+            { text: "ADD", correct: false},
+            { text: "INSERT", correct: true},
+            { text: "UPDATE", correct: false},
+            { text: "MODIFY", correct: false},  
+        ]
+    },
+    {
+        question: "Which statement is used to retrieve data from a MySQL database?",
+        answers: [
+            { text: "GET", correct: false},
+            { text: "FETCH", correct: false},
+            { text: "SELECT", correct: true},
+            { text: "EXTRACT", correct: false},  
+        ]
+    },
+    {
+        question: "What does SQL stand for?",
+        answers: [
+            { text: "Structed Query Language", correct: true},
+            { text: "Simple Question Language", correct: false},
+            { text: "Sequential Query Language", correct: false},
+            { text: "Scripted Question Language", correct: false},  
+        ]
+    },
+    {
+        question: "What does the SQL statement ALTER TABLE do?",
+        answers: [
+            { text: "Deletes a Table", correct: false},
+            { text: "Adds a new column or modifies an existing column in a table", correct: true},
+            { text: " Retrieves data from multiple tables", correct: false},
+            { text: "Combines two tables into a new table", correct: false},  
+        ]
+    },
+    {
+        question: "What is the purpose of the WHERE clause in SQL?",
+        answers: [
+            { text: "To specify the columns to be retrieved", correct: false},
+            { text: "To filter rows based on a condition", correct: true},
+            { text: " To sort the result set", correct: false},
+            { text: " To join two tables", correct: false}, 
+        ]
+
     }
+    
 ];
 const questionElement =document.getElementById("question");
 const answerButtons =document.getElementById("answer-buttons");
@@ -55,6 +120,11 @@ function showQuestion(){
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
+        if (answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+
 
     });
 
@@ -68,6 +138,51 @@ function resetState(){
     }
 }
 
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+
+
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `you scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Restart Quiz";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+
+    }else{
+        showScore();
+    }
+}
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+
+    }else{
+        startQuiz();
+    }
+
+})
 startQuiz();
 
 
